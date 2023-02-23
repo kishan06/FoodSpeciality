807bcbec922033223efcc5c90355b2240cd69f43
rajshinde046 <rajshinde046@gmail.com>
2023-02-23 12:36:43 +0530

    color

--- foodspeciality/lib/utils/colors.dart
+++ foodspeciality/lib/utils/colors.dart
@@ -4,6 +4,10 @@ class AppColors {
   static const white = Color(0xffFFFFFF);
   static const buttonGrey54595F = Color(0xff54595F);
   static const grey707070 = Color(0xff707070);
+  static const grey3B3F43 = Color(0xff3B3F43);
+  static const grey979797 = Color(0xff979797);
+  static const greyLtEBEBEB = Color(0xffEBEBEB);
+
   static const black = Colors.black;
 
   // static const buttonColor = Color(0xff3B82F6);
