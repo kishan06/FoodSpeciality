cac7c313a1c8ca8cf521372e3c4b69a390d0def2
rajshinde046 <rajshinde046@gmail.com>
2023-03-20 13:04:11 +0530

    UI complete

--- foodspeciality/lib/screens/InsideBottomBar/chats/Screens/ChatTabview.dart
+++ foodspeciality/lib/screens/InsideBottomBar/chats/Screens/ChatTabview.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodspeciality/common%20files/app_bar.dart';
+import 'package:foodspeciality/common%20files/comman_app_bar.dart';
 import 'package:foodspeciality/common%20files/comman_tabbar.dart';
 import 'package:foodspeciality/screens/InsideBottomBar/chats/Screens/ChatCommunityPage.dart';
 import 'package:foodspeciality/screens/InsideBottomBar/chats/Screens/ChatPage.dart';
+import 'package:foodspeciality/utils/colors.dart';
 import 'package:get/get.dart';
 
 class ChattabView extends StatefulWidget {
@@ -20,6 +23,7 @@ class _ChattabViewState extends State<ChattabView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar: CustomAppBar(titleTxt: ""),
       key: _key,
       backgroundColor: Color(0xFFFFFFFF),
       body: SingleChildScrollView(
@@ -29,8 +33,26 @@ class _ChattabViewState extends State<ChattabView> {
               length: 2,
               child: Column(
                 children: [
-                  SizedBox(
-                    height: 10,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 10.w, top: 10.h),
+                        child: InkWell(
+                          onTap: () {
+                            Get.back();
+                          },
+                          child: Image.asset(
+                            "assets/icons/back_arrow.png",
+                            height: 20.h,
+                            width: 30.w,
+                            color: AppColors.greyD3B3F43,
+
+                            // color: ,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   CommanTabbar("Private", "Community"),
                   SizedBox(
