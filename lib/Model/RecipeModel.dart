c81f1e4aa76ca3853878bacfa16dea35ca16f7e0
rajshinde046 <rajshinde046@gmail.com>
2023-07-31 11:56:53 +0530

    changes in get recipe

--- lib/Model/RecipeModel.dart
+++ lib/Model/RecipeModel.dart
@@ -4,22 +4,19 @@ class RecipeModel {
 
   RecipeModel({this.success, this.data});
 
-  RecipeModel.fromJson(Map<String, dynamic> json) {
-    success = json['success'];
-    if (json['data'] != null) {
-      data = <Data>[];
-      json['data'].forEach((v) {
-        data!.add(new Data.fromJson(v));
-      });
-    }
+  factory RecipeModel.fromJson(Map<String, dynamic> json) {
+    return RecipeModel(
+      success: json['success'],
+      data: List<Data>.from(
+          (json['data'] ?? []).map((data) => Data.fromJson(data))),
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['success'] = this.success;
-    if (this.data != null) {
-      data['data'] = this.data!.map((v) => v.toJson()).toList();
-    }
+    final Map<String, dynamic> data = {
+      'success': this.success,
+      'data': this.data?.map((data) => data.toJson()).toList(),
+    };
     return data;
   }
 }
@@ -41,78 +38,88 @@ class Data {
   List<Ingredients>? ingredients;
   User? user;
   List<Tags>? tags;
+  int? comments;
+  int? likes;
+  bool? liked;
+  bool? saved;
+  bool? following;
 
-  Data(
-      {this.id,
-      this.video,
-      this.coverImage,
-      this.name,
-      this.description,
-      this.difficulty,
-      this.cookingTime,
-      this.servings,
-      this.status,
-      this.createdAt,
-      this.updatedAt,
-      this.userId,
-      this.publishedStatus,
-      this.ingredients,
-      this.user,
-      this.tags});
+  Data({
+    this.id,
+    this.video,
+    this.coverImage,
+    this.name,
+    this.description,
+    this.difficulty,
+    this.cookingTime,
+    this.servings,
+    this.status,
+    this.createdAt,
+    this.updatedAt,
+    this.userId,
+    this.publishedStatus,
+    this.ingredients,
+    this.user,
+    this.tags,
+    this.comments,
+    this.likes,
+    this.liked,
+    this.saved,
+    this.following,
+  });
 
-  Data.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    video = json['video'];
-    coverImage = json['cover_image'];
-    name = json['name'];
-    description = json['description'];
-    difficulty = json['difficulty'];
-    cookingTime = json['cooking_time'];
-    servings = json['servings'];
-    status = json['status'];
-    createdAt = json['createdAt'];
-    updatedAt = json['updatedAt'];
-    userId = json['userId'];
-    publishedStatus = json['published_status'];
-    if (json['ingredients'] != null) {
-      ingredients = <Ingredients>[];
-      json['ingredients'].forEach((v) {
-        ingredients!.add(new Ingredients.fromJson(v));
-      });
-    }
-    user = json['user'] != null ? new User.fromJson(json['user']) : null;
-    if (json['tags'] != null) {
-      tags = <Tags>[];
-      json['tags'].forEach((v) {
-        tags!.add(new Tags.fromJson(v));
-      });
-    }
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
+      id: json['id'],
+      video: json['video'],
+      coverImage: json['cover_image'],
+      name: json['name'],
+      description: json['description'],
+      difficulty: json['difficulty'],
+      cookingTime: json['cooking_time'],
+      servings: json['servings'],
+      status: json['status'],
+      createdAt: json['createdAt'],
+      updatedAt: json['updatedAt'],
+      userId: json['userId'],
+      publishedStatus: json['published_status'],
+      ingredients: List<Ingredients>.from((json['ingredients'] ?? [])
+          .map((data) => Ingredients.fromJson(data))),
+      user: json['user'] != null ? User.fromJson(json['user']) : null,
+      tags: List<Tags>.from(
+          (json['tags'] ?? []).map((data) => Tags.fromJson(data))),
+      comments: json['comments'],
+      likes: json['likes'],
+      liked: json['liked'],
+      saved: json['saved'],
+      following: json['following'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['video'] = this.video;
-    data['cover_image'] = this.coverImage;
-    data['name'] = this.name;
-    data['description'] = this.description;
-    data['difficulty'] = this.difficulty;
-    data['cooking_time'] = this.cookingTime;
-    data['servings'] = this.servings;
-    data['status'] = this.status;
-    data['createdAt'] = this.createdAt;
-    data['updatedAt'] = this.updatedAt;
-    data['userId'] = this.userId;
-    data['published_status'] = this.publishedStatus;
-    if (this.ingredients != null) {
-      data['ingredients'] = this.ingredients!.map((v) => v.toJson()).toList();
-    }
-    if (this.user != null) {
-      data['user'] = this.user!.toJson();
-    }
-    if (this.tags != null) {
-      data['tags'] = this.tags!.map((v) => v.toJson()).toList();
-    }
+    final Map<String, dynamic> data = {
+      'id': this.id,
+      'video': this.video,
+      'cover_image': this.coverImage,
+      'name': this.name,
+      'description': this.description,
+      'difficulty': this.difficulty,
+      'cooking_time': this.cookingTime,
+      'servings': this.servings,
+      'status': this.status,
+      'createdAt': this.createdAt,
+      'updatedAt': this.updatedAt,
+      'userId': this.userId,
+      'published_status': this.publishedStatus,
+      'ingredients': this.ingredients?.map((data) => data.toJson()).toList(),
+      'user': this.user?.toJson(),
+      'tags': this.tags?.map((data) => data.toJson()).toList(),
+      'comments': this.comments,
+      'likes': this.likes,
+      'liked': this.liked,
+      'saved': this.saved,
+      'following': this.following,
+    };
     return data;
   }
 }
@@ -125,19 +132,22 @@ class Ingredients {
 
   Ingredients({this.id, this.name, this.quantity, this.recipeId});
 
-  Ingredients.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    quantity = json['quantity'];
-    recipeId = json['recipeId'];
+  factory Ingredients.fromJson(Map<String, dynamic> json) {
+    return Ingredients(
+      id: json['id'],
+      name: json['name'],
+      quantity: json['quantity'],
+      recipeId: json['recipeId'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['quantity'] = this.quantity;
-    data['recipeId'] = this.recipeId;
+    final Map<String, dynamic> data = {
+      'id': this.id,
+      'name': this.name,
+      'quantity': this.quantity,
+      'recipeId': this.recipeId,
+    };
     return data;
   }
 }
@@ -160,61 +170,65 @@ class User {
   bool? online;
   String? role;
 
-  User(
-      {this.id,
-      this.createdAt,
-      this.updatedAt,
-      this.profileImage,
-      this.username,
-      this.firstName,
-      this.lastName,
-      this.email,
-      this.phone,
-      this.password,
-      this.bio,
-      this.location,
-      this.socialLinks,
-      this.lastLogin,
-      this.online,
-      this.role});
+  User({
+    this.id,
+    this.createdAt,
+    this.updatedAt,
+    this.profileImage,
+    this.username,
+    this.firstName,
+    this.lastName,
+    this.email,
+    this.phone,
+    this.password,
+    this.bio,
+    this.location,
+    this.socialLinks,
+    this.lastLogin,
+    this.online,
+    this.role,
+  });
 
-  User.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    createdAt = json['createdAt'];
-    updatedAt = json['updatedAt'];
-    profileImage = json['profile_image'];
-    username = json['username'];
-    firstName = json['first_name'];
-    lastName = json['last_name'];
-    email = json['email'];
-    phone = json['phone'];
-    password = json['password'];
-    bio = json['bio'];
-    location = json['location'];
-    socialLinks = json['social_links'];
-    lastLogin = json['last_login'];
-    online = json['online'];
-    role = json['role'];
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      createdAt: json['createdAt'],
+      updatedAt: json['updatedAt'],
+      profileImage: json['profile_image'],
+      username: json['username'],
+      firstName: json['first_name'],
+      lastName: json['last_name'],
+      email: json['email'],
+      phone: json['phone'],
+      password: json['password'],
+      bio: json['bio'],
+      location: json['location'],
+      socialLinks: json['social_links'],
+      lastLogin: json['last_login'],
+      online: json['online'],
+      role: json['role'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['createdAt'] = this.createdAt;
-    data['updatedAt'] = this.updatedAt;
-    data['profile_image'] = this.profileImage;
-    data['username'] = this.username;
-    data['first_name'] = this.firstName;
-    data['last_name'] = this.lastName;
-    data['email'] = this.email;
-    data['phone'] = this.phone;
-    data['password'] = this.password;
-    data['bio'] = this.bio;
-    data['location'] = this.location;
-    data['social_links'] = this.socialLinks;
-    data['last_login'] = this.lastLogin;
-    data['online'] = this.online;
-    data['role'] = this.role;
+    final Map<String, dynamic> data = {
+      'id': this.id,
+      'createdAt': this.createdAt,
+      'updatedAt': this.updatedAt,
+      'profile_image': this.profileImage,
+      'username': this.username,
+      'first_name': this.firstName,
+      'last_name': this.lastName,
+      'email': this.email,
+      'phone': this.phone,
+      'password': this.password,
+      'bio': this.bio,
+      'location': this.location,
+      'social_links': this.socialLinks,
+      'last_login': this.lastLogin,
+      'online': this.online,
+      'role': this.role,
+    };
     return data;
   }
 }
@@ -227,21 +241,22 @@ class Tags {
 
   Tags({this.id, this.recipeId, this.tagId, this.tag});
 
-  Tags.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    recipeId = json['recipeId'];
-    tagId = json['tagId'];
-    tag = json['tag'] != null ? new Tag.fromJson(json['tag']) : null;
+  factory Tags.fromJson(Map<String, dynamic> json) {
+    return Tags(
+      id: json['id'],
+      recipeId: json['recipeId'],
+      tagId: json['tagId'],
+      tag: json['tag'] != null ? Tag.fromJson(json['tag']) : null,
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['recipeId'] = this.recipeId;
-    data['tagId'] = this.tagId;
-    if (this.tag != null) {
-      data['tag'] = this.tag!.toJson();
-    }
+    final Map<String, dynamic> data = {
+      'id': this.id,
+      'recipeId': this.recipeId,
+      'tagId': this.tagId,
+      'tag': this.tag?.toJson(),
+    };
     return data;
   }
 }
@@ -253,17 +268,20 @@ class Tag {
 
   Tag({this.id, this.name, this.tagCategoryId});
 
-  Tag.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    tagCategoryId = json['tag_categoryId'];
+  factory Tag.fromJson(Map<String, dynamic> json) {
+    return Tag(
+      id: json['id'],
+      name: json['name'],
+      tagCategoryId: json['tag_categoryId'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['tag_categoryId'] = this.tagCategoryId;
+    final Map<String, dynamic> data = {
+      'id': this.id,
+      'name': this.name,
+      'tag_categoryId': this.tagCategoryId,
+    };
     return data;
   }
 }
